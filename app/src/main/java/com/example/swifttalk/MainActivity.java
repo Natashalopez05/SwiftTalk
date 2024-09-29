a862269f92dc0dcac168df0de39cbc1610e749fc
Natasha Lopez <nmlc0001@ce.pucmm.edu.do>
2024-09-29 14:45:54 -0400

    feat(main): implement login and register using firebase authentication

--- app/src/main/java/com/example/swifttalk/MainActivity.java
+++ app/src/main/java/com/example/swifttalk/MainActivity.java
@@ -26,53 +26,18 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     EdgeToEdge.enable(this);
-
     setContentView(R.layout.activity_main);
-
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-      return insets;
-    });
-
-    // Firebase Auth
-    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
-      @Override
-      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        if (currentUser != null) {
-          Log.d("AuthStateListener", "User :" + currentUser.getEmail());
-        }
-      }
-    };
-
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    auth.addAuthStateListener(authStateListener);
-    //FirebaseUser currentUser = auth.getCurrentUser();
 
     Button loginButton = findViewById(R.id.login_button);
     loginButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent intent = new Intent(MainActivity.this, LogInActivity.class);
-        auth.createUserWithEmailAndPassword("vladimir@ce.pucmm.edu.do", "123456")
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                  @Override
-                  public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                      FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                      Log.i("AuthStateListener", "User: " + user.getEmail());
-                      Toast.makeText(MainActivity.this, "Logged in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
-                    }
-                  }
-                });
         startActivity(intent);
       }
     });
 
-    // Register button action
     Button registerButton = findViewById(R.id.register_button);
     registerButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -81,13 +46,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
       }
     });
-    
 
-
-
-
-
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+      return insets;
+    });
   }
-
-
 }
--- app/src/main/java/com/example/swifttalk/RegisterActivity.java
+++ app/src/main/java/com/example/swifttalk/RegisterActivity.java
@@ -1,20 +1,62 @@
 package com.example.swifttalk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+  EditText nameEdit, lastnameEdit, emailEdit, passwordEdit;
+  Button registerButton;
+  FirebaseAuth auth = FirebaseAuth.getInstance();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     EdgeToEdge.enable(this);
-
     setContentView(R.layout.activity_register);
+
+    nameEdit = findViewById(R.id.name_field);
+    lastnameEdit = findViewById(R.id.last_name_field);
+    emailEdit = findViewById(R.id.email_field);
+    passwordEdit = findViewById(R.id.password_field);
+    registerButton = findViewById(R.id.register_button);
+
+    registerButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String name = nameEdit.getText().toString().trim();
+        String lastname = lastnameEdit.getText().toString().trim();
+        String email = emailEdit.getText().toString().trim();
+        String password = passwordEdit.getText().toString().trim();
+
+        if (name.isEmpty() || lastname.isEmpty() || email.isEmpty() || password.isEmpty()) {
+          Toast.makeText(RegisterActivity.this, "Por favor introduce todos los campos", Toast.LENGTH_SHORT).show();
+          return;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+          .addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+              Toast.makeText(RegisterActivity.this, "Error al registrar el usuario", Toast.LENGTH_SHORT).show();
+              return;
+            }
+
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(RegisterActivity.this, "Usuario registrado: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+            finish();
+          });
+      }
+    });
 
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register), (v, insets) -> {
       Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
